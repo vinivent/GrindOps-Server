2b216ead91a82d1923f53c194bf48db7021012db
vinivent <vini.ventura999@gmail.com>
2024-12-20 16:10:22 -0300

    trocando para @RestController

--- src/main/java/com/devent/cprogress/controller/AchievementController.java
+++ src/main/java/com/devent/cprogress/controller/AchievementController.java
@@ -6,12 +6,11 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/achievements")
 public class AchievementController {
 
--- src/main/java/com/devent/cprogress/controller/UserProgressController.java
+++ src/main/java/com/devent/cprogress/controller/UserProgressController.java
@@ -7,12 +7,12 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/user-progress")
 public class UserProgressController {
 
