010d01d715c3356cc6e93c6dec7954375cce0541
vinivent <vini.ventura999@gmail.com>
2024-12-20 16:10:31 -0300

    configuracoes de seguranca

--- src/main/java/com/devent/cprogress/config/security/SecurityConfig.java
+++ src/main/java/com/devent/cprogress/config/security/SecurityConfig.java
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -16,17 +21,40 @@ public class SecurityConfig {
     }
 
     @Bean
+    public InMemoryUserDetailsManager userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        UserDetails user = User.withUsername("admin")
+                .password(bCryptPasswordEncoder.encode("admin")) // Encode the password!
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users", "/api/achievements", "/api/categories",
-                                "/api/camouflages", "/api/challenges", "/api/user-progress")
-                        .permitAll() // Torna essas rotas públicas
-                        .anyRequest().authenticated() // Outras rotas exigem autenticação
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().denyAll()
                 )
-                .httpBasic(Customizer.withDefaults()); // Habilita autenticação HTTP básica
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:8080", "http://localhost:3306") // Replace with your allowed origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*") // Or specify allowed headers
+                        .allowCredentials(true);
+            }
+        };
     }
 }
