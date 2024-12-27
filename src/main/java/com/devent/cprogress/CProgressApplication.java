ed414d2d7a594f604d7d056ef65f645ebcccdf2b
vinivent <vini.ventura999@gmail.com>
2024-12-27 14:15:50 -0300

    teste ComponentScan

--- src/main/java/com/devent/cprogress/CProgressApplication.java
+++ src/main/java/com/devent/cprogress/CProgressApplication.java
@@ -2,8 +2,10 @@ package com.devent.cprogress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.devent.cprogress")
 public class CProgressApplication {
 
 	public static void main(String[] args) {
